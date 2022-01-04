9e568d4120766f0daf6f132dfc0df666dfa22772
thibautbaissac <tbaissac@gmail.com>
2022-01-04 13:12:29 +0100

    Set pg_search in model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search, against: [:title, :release, :price, :genre], using: {tsearch: {prefix: true}}
+
+  PRICE_RANGES = [[0, 5], [5, 5], [15, 25], [25, 100000]]
 end
